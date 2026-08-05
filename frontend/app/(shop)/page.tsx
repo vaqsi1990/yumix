be67f180368c4f4fb999a84de1d18f7f6b272d93
vaqsi1990 <vaqsii23@gmail.com>
2026-08-05 12:43:44 +0400

    log

--- frontend/app/(shop)/page.tsx
+++ frontend/app/(shop)/page.tsx
@@ -1,7 +1,7 @@
 import Category from "@/components/Category";
 import FavoriteFood from "@/components/FavoriteFood";
 import Rest from "@/components/Rest";
-
+console.log("Home");
 export default function Home() {
   return (
     <>
